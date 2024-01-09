0c9293425c5f26be730ae98832322506fc2ad554
concept <conceptcodes@gmail.com>
2024-01-08 21:00:48 -0500

    init

---
+++ src/test/java/com/zoomies/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoomies.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
